139f29b890341e6768af3adc8e44caf9662a1171
Kevin Blair <kblair40@gmail.com>
2023-02-08 09:49:09 -0600

    Add readFileStream parse helper function

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, StreamableFile } from '@nestjs/common';
+import { parse } from 'csv-parse';
 import * as nodemailer from 'nodemailer';
+import { createReadStream, readFile } from 'fs';
+import { join } from 'path';
 // for some reason, nodemailer is undefined if imported like... import nodemailer from 'nodemailer';
 // https://stackoverflow.com/questions/27084159/cannot-read-property-createtransport-of-undefined
+
+// import file from '../assets/budget.csv';
 
 const EMORY_PASSWORD = process.env.EMORY_PASSWORD;
 const EMORY_NETID = process.env.EMORY_NETID;
@@ -35,5 +40,50 @@ export class EmailService {
 
     // Preview only available when sending through an Ethereal account
     console.log('Preview URL:', nodemailer.getTestMessageUrl(email));
+  }
+
+  parseCSV() {
+    // const file = createReadStream(join(process.cwd(), 'assets/budget.csv'));
+    // const file = readFile(join(process.cwd(), 'assets/budget.csv'), (data) =>
+    //   console.log('\nDATA IN CB:', data),
+    // );
+    // const streamableFile = new StreamableFile(file);
+    // console.log('\nSTREAMABLEFILE:', streamableFile);
+    // console.log('\nSTREAM:', streamableFile.getStream());
+
+    createReadStream(join(process.cwd(), 'assets/budget.csv'))
+      .pipe(parse({ delimiter: '|', relax_quotes: true }))
+      .on('data', function (row) {
+        console.log('\n\nROW:', row);
+      });
+    // const file = createReadStream(join(process.cwd(), 'assets/budget.csv'))
+    //   .pipe(parse({ delimiter: '|' }))
+    //   .on('data', function (row) {
+    //     console.log('\n\nROW:', row);
+    //   });
+
+    const records = [];
+    // const parser = parse({
+    //   delimiter: '|',
+    // });
+
+    // parser.on('readable', function () {
+    //   let record;
+    //   while ((record = parser.read()) !== null) {
+    //     console.log('record:', record);
+    //     records.push(record);
+    //   }
+    // });
+
+    // parser.on('end', function () {
+    //   console.log('\n\nENDED!!\n\n');
+    // });
+
+    // parser.write(file);
+
+    // return streamableFile;
+    // console.log('\n\nFILE:', file);
+
+    return 'Hello';
   }
 }
